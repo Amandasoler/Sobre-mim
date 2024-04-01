433aafae37f18e7d419175208699895de7fc6064
Amanda Soler <163319301+Amandasoler@users.noreply.github.com>
2024-04-01 15:05:56 -0300

    Commit Inicial

---
+++ cv-index.py
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<!-- @falvojr: Reference article https://www.w3schools.com/howto/howto_website_create_resume.asp -->
+<!-- @falvojr: Useful link to use fontawesome (fa) icons: https://fontawesome.com/v4/icons/ -->
+<html lang="pt-BR">
+
+  <head>
+    <title>Amanda Soler</title>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+      html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+    </style>
+    
+  </head>
+
+  <body class="w3-light-grey" cz-shortcut-listen="true">
+  
+    <!-- Page Container -->
+    <div class="w3-content w3-margin-top" style="max-width:1400px;">
+  
+      <!-- The Grid -->
+      <div class="w3-row-padding">
+  
+        <!-- Left Column -->
+        <div class="w3-third">
+  
+          <div class="w3-white w3-text-grey w3-card-4">
+            <!-- @falvojr: This container has been modified to not use a profile picture. -->
+            <div class="w3-container">
+              <h2>Amanda Soler Fonseca</h2>
+            </div>
+            <div class="w3-container">
+              <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Graduanda em Inteligência Artificial</p>
+              <p><i class="fa fa-heart-o fa-fw w3-margin-right w3-large w3-text-teal"></i>Mãe do Matheus e da Alice</p>
+              <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Mogi das Cruzes/SP</p>
+              <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>soler.amanda@gmail.com</p>
+              <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(11) 93355-8544</p>
+              <hr>
+  
+              <p class="w3-large">
+                <b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b>
+              </p>
+              <p>HTML</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:40%">30%</div>
+              </div>
+              <p>Javascript</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:40%">30%</div>
+              </div>
+              <p>SQL</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:30%">40%</div>
+              </div>
+              <p>Python</p>
+              <div class="w3-light-grey w3-round-xlarge w3-small">
+                <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+              </div>
+              <br>
+  
+              <p class="w3-large w3-text-theme">
+                <b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Idiomas</b>
+              </p>
+              <p>Inglês</p>
+              <div class="w3-light-grey w3-round-xlarge">
+                <div class="w3-round-xlarge w3-teal" style="height:24px;width:30%"></div>
+              </div>
+              <br>
+            </div>
+          </div><br>
+  
+          <!-- End Left Column -->
+        </div>
+  
+        <!-- Right Column -->
+        <div class="w3-twothird">
+    
+          <div class="w3-container w3-card w3-white w3-margin-bottom">
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-user-circle-o fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Sobre</h2>
+            <p>Aos 35 anos, sou uma mulher independente, mãe de dois filhos incríveis e possuo experiência em diversas áreas de negócio,
+               além de facilidade com o aprendizado de novas ferramentas de tecnologia.<br> 
+               Através do aprendizado contínuo em Análise de dados e Inteligência Artificial, venho desenvolvendo habilidades técnicas, com o objetivo de me recolocar profissionalmente, 
+               contribuindo com grandes projetos e atuando para uma sociedade mais justa e inclusiva.<br> 
+               Gosto de desafios e de solucionar problemas de forma prática e criativa. Sou apaixonada por inovação e motivada pela oportunidade de ajudar as pessoas.</p>
+          </div>
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom">
+            <h2 class="w3-text-grey w3-padding-16">
+              <i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Experiências
+            </h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>ROTA Z Chatbots Integrados</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Janeiro de 2023 - <span class="w3-tag w3-teal w3-round">Atual</span></h6>
+              <p>Experiências com marketing, analise de requisitos, gestão de projetos, consultoria, atendimento ao cliente, gestão de CRM, e-commerce, 
+                qualificação de leads, relatórios, testes, automação de chatbots e apresentações.</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>CV Marketplace e Intermediação de Negócios Ltda</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Janeiro de 2018 - Dezembro de 2020</h6>
+              <p>Experiências com criação de startup, experiência do usuário, liderança de equipe e implementação de estratégias de 
+                recrutamento, seleção e treinamento de equipe de atendimento e backoffice, vendas de e-commerce, marketing, gestão de catálogo e solução de problemas.</p>
+            </div>
+          </div>
+  
+          <div class="w3-container w3-card w3-white">
+            <h2 class="w3-text-grey w3-padding-16">
+              <i class="fa fa-graduation-cap fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Educação
+            </h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Digital Innovation One (DIO)</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Março de 2024 - <span class="w3-tag w3-teal w3-round">Atual</span></h6>
+              <p>
+                Bootcamp: Python para Data Analytcs (68 horas)
+              </p>
+              <ul>
+                <li><a href="https://www.dio.me/certificate/PSVCWRIO" target="_blank">Desafios de Código: Aperfeiçoe sua Lógica e Pensamento Computacional</a></li>
+                <li><a href="https://www.dio.me/certificate/RPQFI1GS" target="_blank">Contribuindo em um Projeto OpenSource no GitHub</a></li>
+              </ul>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Centro Universitário Braz Cubas</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2023 - <span class="w3-tag w3-teal w3-round">Atual</span></h6>
+              <p>CST em Inteligência Artificial</p>
+            </div>
+          </div>
+
+          <div class="w3-container w3-card w3-white w3-margin-bottom">
+            <h2 class="w3-text-grey w3-padding-16">
+              <i class="fa fa-git fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Projetos
+            </h2>
+            <div class="w3-container">
+              <h6 class="w3-text-teal">
+                <i class="fa fa-github fa-fw w3-margin-right"></i>
+                <a href="https://github.com/Amandasoler" target="_blank">Amandasoler/Sobre-mim</a>
+              </h6>
+              <p>Em construção.</p>
+              <hr>
+            </div>
+          </div>
+  
+          <!-- End Right Column -->
+        </div>
+  
+        <!-- End Grid -->
+      </div>
+  
+      <!-- End Page Container -->
+    </div>
+  
+    <footer class="w3-container w3-teal w3-center w3-margin-top">
+      <p>Minhas Redes Sociais</p>
+      <a href="https://github.com/Amandasoler" target="_blank"><i class="fa fa-github w3-hover-opacity"></i></a>
+      <a href="https://linkedin.com/in/amanda-soller" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+    </footer>
+  
+  </body>
+
+</html>
